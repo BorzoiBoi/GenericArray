c345068741934528a02df1229f706505f513efc8
Łukasz <lukas.rutana@gmail.com>
2023-03-21 13:17:00 +0100

    -length +toString()
    Removed length variable, as it was basically useless, and changed arrWrite() to toString() function for it to be much more Universal

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,26 @@ public class Main {
         System.out.println("\nArray Length: " + str_Array.size());
         str_Array.add("elem11");
         str_Array.add("elem12");
-        str_Array.arrWrite();
+        System.out.println(str_Array);
         str_Array.clear();
         System.out.println("post-clear length: " + str_Array.size());
-        str_Array.arrWrite();
+        System.out.println(str_Array);
         System.out.println("post-clear get 0: " + str_Array.get(0));
         str_Array.add("elem0");
         System.out.println("post-add get 0: " + str_Array.get(0));
         str_Array.add("elem1");
         str_Array.add(0, "elem2");
-        str_Array.arrWrite();
+        System.out.println(str_Array);
         System.out.println("pop! : " + str_Array.pop());
         str_Array.add("elem3");
-        str_Array.arrWrite();
+        System.out.println(str_Array);
         System.out.println("pop! : " + str_Array.pop());
         System.out.println("pop! : " + str_Array.pop());
         str_Array.add("elem3");
         str_Array.add("elem4");
         str_Array.add("elem5");
-        str_Array.arrWrite();
+        System.out.println(str_Array);
         str_Array.remove(2);
-        str_Array.arrWrite();
+        System.out.println(str_Array);
     }
 }
--- src/MyCustomList.java
+++ src/MyCustomList.java
@@ -1,33 +1,30 @@
 public class MyCustomList<T> implements IMyList<T> {
     private T[] objArray;
-    public int length;
     private final Class<T> dataType;
     //WE GOT TEK!
     public MyCustomList(Class<T> dataType){
         this.dataType = dataType;
         this.objArray = (T[]) java.lang.reflect.Array.newInstance(dataType, 0);
-        this.length = 0;
     }
     public T get(int i) {
-        if(this.length == 0)
+        if(objArray.length == 0)
           return null;
         return objArray[i];
     }
 
     public void add(T element) {
-        T[] tempArray = (T[]) java.lang.reflect.Array.newInstance(dataType, length + 1);
-        for(int i = 0; i < length; i++)
+        T[] tempArray = (T[]) java.lang.reflect.Array.newInstance(dataType, objArray.length + 1);
+        for(int i = 0; i < objArray.length; i++)
         {
             tempArray[i] = objArray[i];
         }
-        tempArray[length] = element;
-        this.length++;
+        tempArray[objArray.length] = element;
         objArray = tempArray;
     }
 
     public void add(int index, T element) {
-        T[] tempArray = (T[]) java.lang.reflect.Array.newInstance(dataType, length + 1);
-        for(int i = 0; i < length+1; i++)
+        T[] tempArray = (T[]) java.lang.reflect.Array.newInstance(dataType, objArray.length + 1);
+        for(int i = 0; i < objArray.length+1; i++)
         {
             if(i < index) {
               tempArray[i] = objArray[i];
@@ -39,26 +36,23 @@ public class MyCustomList<T> implements IMyList<T> {
               tempArray[i] = objArray[i-1];
             }
         }
-        this.length++;
         objArray = tempArray;
     }
 
     public void clear() {
         this.objArray = (T[]) java.lang.reflect.Array.newInstance(this.dataType, 0);
-        this.length = 0;
     }
 
     public T pop() {
-        if(this.length == 0)
+        if(objArray.length == 0)
           return null;
-        T tempElem = objArray[length-1];
-        T[] tempArray = (T[]) java.lang.reflect.Array.newInstance(this.dataType, length-1);
-        for(int i = 0; i < length-1; i++)
+        T tempElem = objArray[objArray.length-1];
+        T[] tempArray = (T[]) java.lang.reflect.Array.newInstance(this.dataType, objArray.length-1);
+        for(int i = 0; i < objArray.length-1; i++)
         {
             tempArray[i] = objArray[i];
         }
         objArray = tempArray;
-        this.length--;
         return tempElem;
     }
 
@@ -67,11 +61,11 @@ public class MyCustomList<T> implements IMyList<T> {
     }
 
     public T remove(int index) {
-        if(index < 0 || index >= length)
+        if(index < 0 || index >= objArray.length)
           return null;
         T tempElem = objArray[index];
-        T[] tempArray = (T[]) java.lang.reflect.Array.newInstance(this.dataType, length-1);
-        for(int i = 0; i < length-1; i++)
+        T[] tempArray = (T[]) java.lang.reflect.Array.newInstance(this.dataType, objArray.length-1);
+        for(int i = 0; i < objArray.length-1; i++)
         {
             if(i < index) {
                 tempArray[i] = objArray[i];
@@ -81,20 +75,20 @@ public class MyCustomList<T> implements IMyList<T> {
             }
         }
         objArray = tempArray;
-        this.length--;
         return tempElem;
     }
 
-    public void arrWrite() {
-        if(length == 0)
+    public String toString() {
+        if(objArray.length == 0)
         {
-            System.out.print("[]\n");
-            return;
+            return "[]";
         }
-        System.out.print("[");
-        for (int i = 0; i < length-1; i++) {
-            System.out.print(objArray[i] + ", ");
+        String ret = "";
+        ret += "[";
+        for (int i = 0; i < objArray.length-1; i++) {
+            ret += objArray[i] + ", ";
         }
-        System.out.print(objArray[length-1] + "]\n");
+        ret += objArray[objArray.length-1] + "]\n";
+        return ret;
     }
 }
